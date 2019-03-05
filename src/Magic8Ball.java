34de5f5b846c4f45adcf74908a5992b7f2727611
Bharat <bharat.paudel.2019@student.lhschools.org>
2019-03-05 07:57:12 -0500

    Added more variables in the declare phase and made a initialize phase

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -6,7 +6,20 @@ public class Magic8Ball {
         //declare
         Scanner keyboard;
         int testing;
-        String question; 
+        String question;
+        boolean play = false;
+        String Choice;
+
+
+
+        //intialization phase
+        keyboard = new Scanner(System.in);
+        do {
+            System.out.println("Hello, I am the Magic8Ball!\nWhat answers do you seek from me?");
+            System.out.println("Ask me any yes or no question to learn about your fate!");
+            question = keyboard.nextLine();
+            testing = (int) (Math.random() * 100);
+            System.out.println(question);
 
 
 

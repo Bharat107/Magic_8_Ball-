2d15abdcc56b48a8f7d1a7729eaf205b9b41b0e7
Bharat <bharat.paudel.2019@student.lhschools.org>
2019-03-05 07:46:20 -0500

    Declared my variables

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Magic8Ball {
 
     public static void main(String[]args){
-        
+        //declare
+        Scanner keyboard;
+        int testing;
+        String question; 
 
 
 
